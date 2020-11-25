2189c5290fb93b44e299742ef929da247e9bf088
mcdanton <danhefter@gmail.com>
2020-11-24 19:15:40 -0500

    Added new classes to manifest

--- app/src/main/java/com/recipe_book/RecommendFoodActivity.kt
+++ app/src/main/java/com/recipe_book/RecommendFoodActivity.kt
@@ -1,8 +1,8 @@
 package com.recipe_book
 
-import android.app.Application
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.recipe_book.R
 import javax.inject.Inject
 
 class RecommendFoodActivity: AppCompatActivity() {
@@ -12,6 +12,7 @@ class RecommendFoodActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_suggest_meal)
 
         (application as App).appComponent.inject(this)
     }
